f82f5d23706ef4ab5d6e2c23f792012fb3c4e08e
Ivan Kirshak <i.kirshak@gmail.com>
2020-04-20 11:46:15 +0300

    another trial to deploy to gh-pages

--- ivan-kirshak.github.io/src/App.js
+++ ivan-kirshak.github.io/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './css/App.css';
+import './App.css';
 
 function App() {
   return (
@@ -8,8 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This page is under construction using <code>create-react-app</code>.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
