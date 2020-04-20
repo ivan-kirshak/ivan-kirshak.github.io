c0b5197a755b414c7af94a4a1151290c010bb800
Ivan Kirshak <i.kirshak@gmail.com>
2020-04-20 11:02:59 +0300

    first commit

---
+++ ivan-kirshak.github.io/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This page is under construction using <code>create-react-app</code>.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
